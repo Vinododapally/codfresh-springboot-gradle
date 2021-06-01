8f40ba1501ad7c303dbacac0aeacf24a11f7ca67
vinod <odapallyvinod@gmai.com>
2021-06-01 13:39:13 +0530

    first commit

---
+++ src/test/java/gradleexample/GradleExampleApplicationTest.java
@@ -0,0 +1,19 @@
+package gradleexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleExampleApplicationTest {
+
+	@Test
+	public void contextLoads() {
+		assertEquals("Expected correct message","Hello World","Hello "+"World");
+	}
+
+}
